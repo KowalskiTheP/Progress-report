found a bug in load data that screwed with our predictions, some maintenance in main.py, added inverse scaling for plotting, new function in loadData that makes the windows etc

Src/loadData.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 from datetime import datetime
+import sys
 
 ## loading data from a CSV file in a pandas DataFrame. 
 ## The diff between the values in dateColumn and the refdate gets also calculated and added.
@@ -24,13 +25,22 @@ def load_fromCSV(csvFile, decimal , seperator, header, dateColumn):
 
 ## Creating a numpy array with all data from the selected columns 
 ## of the previosly initialised dataFrame
-def getDataSet(dataframe, columns):
+def getDataSet(dataframe, columns, trainTestSplit):
   dataSet = np.zeros((len(dataframe), len(columns)))
   for i in range(len(columns)):
     dataSet[:,i] = dataframe.iloc[:,columns[i]]
   
-  trainSet, testSet = train_test_split(dataSet, test_size = 0.3)
+  ############### maybe dont split train and test set randomly??? sounds crazy with a time series?
+  #trainSet, testSet = train_test_split(dataSet, test_size = trainTestSplit)
   
+  # first try last X percent of the dataset are the test set. PLEASE REVIEW!!!!
+  # In principle it works now...
+  lastXpercent = int(np.floor(len(dataframe)*trainTestSplit))
+  firstXpercent = len(dataframe) - lastXpercent
+  
+  trainSet = dataSet[0:firstXpercent]
+  testSet = dataSet[firstXpercent:firstXpercent+lastXpercent]
+
   return trainSet, testSet
 
 
@@ -56,7 +66,22 @@ def normalise_data(x_data, y_data):
   scaler.fit(x_data)
   x_scaled = scaler.transform(x_data)
   y_scaled = scaler.transform(y_data)
-  return x_scaled, y_scaled
+  return x_scaled, y_scaled, scaler
+
+
+def make_windowed_data(dataframe, config):
+  ''' makes the windowed dataset from a appropriate dataframe'''
+  
+  dataSetTrain, dataSetTest = getDataSet(dataframe, config['columns'], float(config['traintestsplit']))
+  dataSetTrain, dataSetTest, scaler = normalise_data(dataSetTrain, dataSetTest)
+  #print dataSetTrain
+  #print dataSetTest
+  x_fullTrain, y_fullTrain = shiftData(dataSetTrain, config['y_column'], int(config['look_back']))
+  x_winTrain, y_winTrain = get_windows(x_fullTrain,y_fullTrain,int(config['winlength']))
+  x_fullTest, y_fullTest = shiftData(dataSetTest, config['y_column'], int(config['look_back']))
+  x_winTest, y_winTest = get_windows(x_fullTest,y_fullTest,int(config['winlength']))
+  
+  return x_winTrain, y_winTrain, x_winTest, y_winTest, scaler
   
 
 ## Following lines were used to test the functions  

Src/main.py
@@ -4,8 +4,9 @@ import loadData
 
 import time
 import numpy as np
+import sys
 
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 config = readConf.readINI("../Data/config.conf")
 
@@ -17,76 +18,45 @@ print '> Loading data... '
   
 dataframe = loadData.load_fromCSV(config['csvfile'], ',', ';', int(config['header']), config['datecolumn'])
 
-dataSetTrain, dataSetTest = loadData.getDataSet(dataframe, config['columns'])
-x_fullTrain, y_fullTrain = loadData.shiftData(dataSetTrain, config['y_column'], int(config['look_back']))
-x_winTrain, y_winTrain = loadData.get_windows(x_fullTrain,y_fullTrain,int(config['winlength']))
-x_fullTest, y_fullTest = loadData.shiftData(dataSetTest, config['y_column'], int(config['look_back']))
-x_winTest, y_winTest = loadData.get_windows(x_fullTest,y_fullTest,int(config['winlength']))
-
-#print np.shape(x_win)
-#print np.shape(y_win)
-#print x_win[0,:,0]
-
-x_tmp, y_tmp = [], []
-for i in range(len(x_winTrain)):
-  x_tmp_scaled, y_tmp_scaled = loadData.normalise_data(x_winTrain[i,:,0], y_winTrain[i])
-  x_tmp.append(x_tmp_scaled)
-  y_tmp.append(y_tmp_scaled)
-x_win_scaledTrain = np.array(x_tmp).reshape(len(x_tmp),int(config['winlength']),1)
-y_win_scaledTrain = np.array(y_tmp).reshape(len(x_tmp),1)
-
-x_tmp, y_tmp = [], []
-for i in range(len(x_winTest)):
-  x_tmp_scaled, y_tmp_scaled = loadData.normalise_data(x_winTest[i,:,0], y_winTest[i])
-  x_tmp.append(x_tmp_scaled)
-  y_tmp.append(y_tmp_scaled)
-x_win_scaledTest = np.array(x_tmp).reshape(len(x_tmp),int(config['winlength']),1)
-y_win_scaledTest = np.array(y_tmp).reshape(len(x_tmp),1)
-
-print x_win_scaledTrain
-print y_win_scaledTrain
+if config['windoweddata'] == 'on':
   
+  print '> Windowing data..be patient little Padawan'
+  x_winTrain, y_winTrain, x_winTest, y_winTest, scaler = loadData.make_windowed_data(dataframe, config)
+
+else:
+  print 'not implemented so far, exiting!'
+  sys.exit()
+
 print '> Data loaded! This took: ', time.time() - loadData_start_time, 'seconds'
 
 # build the specified model
 model1 = model.build_model(config)
 
 # train the model
-model1.fit(x_win_scaledTrain, y_win_scaledTrain, int(config['batchsize']), int(config['epochs']))
+model1.fit(x_winTrain, y_winTrain, int(config['batchsize']), int(config['epochs']))
 
 # simple predictions or eval metrics
-y_win_scaledTest = y_win_scaledTest.flatten()
-y_win_scaledTrain = y_win_scaledTrain.flatten()
+y_winTest = y_winTest.flatten()
+y_winTrain = y_winTrain.flatten()
 
 if config['evalmetrics'] == 'on':
-    
-    predTest = model.eval_model(x_win_scaledTest, y_win_scaledTest, model1, config, 'test data')
-    predTrain = model.eval_model(x_win_scaledTrain, y_win_scaledTrain, model1, config, 'train data')
+  predTest = model.eval_model(x_winTest, y_winTest, model1, config, 'test data')
+  predTrain = model.eval_model(x_winTrain, y_winTrain, model1, config, 'train data')
     
 else:
-
-    predTest = model.predict_point_by_point(model1, x_win_scaledTest)
-    predTrain = model.predict_point_by_point(model1, x_win_scaledTrain)
-    print np.column_stack((pred, y_win_scaledTest))
+  predTest = model.predict_point_by_point(model1, x_winTest)
+  predTrain = model.predict_point_by_point(model1, x_winTrain)
+  print np.column_stack((pred, y_winTest))
 
 
 if config['plotting'] == 'on':
-    model.plot_data(y_win_scaledTest, predTest)
-    model.plot_data(y_win_scaledTrain, predTrain)
+  
+  predTest = scaler.inverse_transform(predTest)
+  y_winTest = scaler.inverse_transform(y_winTest)
+  y_winTrain = scaler.inverse_transform(y_winTrain)
+  predTrain = scaler.inverse_transform(predTrain)
+  
+  model.plot_data(y_winTest, predTest)
+  model.plot_data(y_winTrain, predTrain)
 
-#trainPredict = model.predict_point_by_point(model1, x_win_scaledTrain)
-#testPredict = model.predict_point_by_point(model1, x_win_scaledTest)
 
-
-
-#plt.plot(y_win_scaledTrain)
-#plt.plot(trainPredict)
-#axes = plt.gca()
-#axes.set_ylim([-0.1,1.1])
-#plt.show()
-
-#plt.plot(y_win_scaledTest)
-#plt.plot(testPredict)
-#axes = plt.gca()
-#axes.set_ylim([-0.1,1.1])
-#plt.show()